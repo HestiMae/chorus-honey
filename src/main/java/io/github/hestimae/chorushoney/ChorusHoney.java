4caad1da0311965a38799419ce4d5faead9de602
HestiMae <hestia.heartstrings@gmail.com>
2025-08-13 14:39:56 +1000

    initial commit

--- src/main/java/io/github/hestimae/chorushoney/ChorusHoney.java
+++ src/main/java/io/github/hestimae/chorushoney/ChorusHoney.java
@@ -1,15 +1,15 @@
-package io.github.username.modid;
+package io.github.hestimae.chorushoney;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ModId implements ModInitializer {
-	public static final String ID = "mod_id";
+public class ChorusHoney implements ModInitializer {
+	public static final String ID = "chorus_honey";
 	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
 
 	@Override
 	public void onInitialize() {
-		//LOGGER.info("[Mod ID] pretty pink princess ponies prancing perpendicular");
+		LOGGER.info("[Chorus Honey] Bzzzzzz... Choruses ur honey");
 	}
 }
